096aadf24af32b593496a6992316d6183a2fe8e2
Mikel Alejo <mikelalejobr@outlook.com>
2019-05-20 12:07:11 +0200

    Add web socket authentication

---
+++ src/main/java/org/onekin/authentication/AuthenticationServiceWebSocket.java
@@ -0,0 +1,25 @@
+package org.onekin.authentication;
+
+import org.onekin.authentication.AuthenticationServiceInterface;
+import org.onekin.authentication.InvalidCredentialsException;
+
+/**
+ * Provides plain authentication
+ */
+class AuthenticationServiceWebSocket implements AuthenticationServiceInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public boolean authenticate(String username, String password) throws InvalidCredentialsException
+    {
+        // stablishes connection to a web socket and tries authenticating.
+        // the below code mocks that behavior
+
+        if (username.equals("user") && password.equals("password")) {
+            return true;
+        } else {
+            throw new InvalidCredentialsException("The provided username or password are invalid");
+        }
+    }
+}
