b0285b9717857e66919043e6160dc9e99907d95c
Alice <alice@example.com>
2019-05-28 12:16:44 +0200

    Implement OpenID authentication

---
+++ src/main/java/org/onekin/authentication/AuthenticationServiceOpenID.java
@@ -0,0 +1,22 @@
+package org.onekin.authentication;
+
+import org.onekin.authentication.AuthenticationServiceInterface;
+import org.onekin.authentication.InvalidCredentialsException;
+
+/**
+ * Provides OpenID authentication
+ */
+class AuthenticationServiceOpenID implements AuthenticationServiceInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public boolean authenticate(String username, String password) throws InvalidCredentialsException
+    {
+        if (username.equals("user") && password.equals("password")) {
+            return true;
+        } else {
+            throw new InvalidCredentialsException("The provided username or password are invalid");
+        }
+    }
+}
